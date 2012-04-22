0b5fa2b873d4c418c4f8d5e070fba676bf5ed891
David Vetrano <dvetrano@stanford.edu>
2012-04-22 23:25:19 +0000

    empty

--- map_article.py
+++ map_article.py
@@ -9,7 +9,7 @@ import sys
 # Schema defined at : 
 #https://github.com/whym/RevDiffSearch/blob/master/README.rst
 get = {'rev_id':0, 'page_id':1, 'namespace':2, 'title':3, 'timestamp':4, 
-'comment':5, 'minor':6, 'page_id':7, 'page_text':8, 'added_size':9, 
+'comment':5, 'minor':6, 'user_id':7, 'user_text':8, 'added_size':9, 
 'removed_size':10, 'added':11, 'removed':12, 'action':13}
 
 # count the edits per page
