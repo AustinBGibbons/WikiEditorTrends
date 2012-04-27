01537e87ec12e361f0b1634905d4631cc8f2b54e
David Vetrano <dvetrano@stanford.edu>
2012-04-27 11:04:50 +0000

    few fixes to the launch script

--- launch-emr.sh
+++ launch-emr.sh
@@ -11,8 +11,8 @@ if [ -z "$2" ]; then
 fi
 
 echo "push files to s3"
-s3cmd put *.py s3://mr-code.wiki/
+s3cmd put *.py s3://mr-code-wiki/
 
 echo "launch emr job"
-/home/ubuntu/emr/elastic-mapreduce --create --name "user-$1" --stream --input s3://diffdb-wiki/ --output "s3://diffdb-output/output-$1/" --mapper s3://mr-code-wiki/map_user.py --reducer s3://mr-code-wiki/reducer_user.py --num-instances "$2" --master-instance-type "c1.medium"  --slave-instance-type "m1.xlarge"
+/home/ubuntu/emr/elastic-mapreduce --create --name "user-$1" --stream --input s3://diffdb-wiki/ --output "s3://diffdb-output/output-$1/" --mapper s3://mr-code-wiki/map_user.py --reducer s3://mr-code-wiki/reducer_user.py --num-instances "$2" --master-instance-type "c1.medium"  --slave-instance-type "m1.xlarge" --enable-debugging
 
