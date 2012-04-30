53b0ee5c604b9dcd92ec976a383f84d665866eb7
David Vetrano <dvetrano@stanford.edu>
2012-04-30 16:11:15 +0000

    Serial Script to Split time segments into their own files

---
+++ timeSplitter.py
@@ -0,0 +1,24 @@
+import sys, os
+
+fileName = 'dataset-all-timesplitters2.tsv'
+if len(sys.argv) > 1 :
+	fileName = sys.argv[1] 
+orig = open(fileName, 'r')
+
+directory = fileName.split('.')[0] + '/'
+if os.path.isdir(directory) :
+	print 'Looks like the data already exists!'
+	sys.exit(0)
+
+os.mkdir(directory)
+
+fileList = {}
+for line in orig :
+	line.rstrip('\n')
+	(user, tab, data) = line.partition('\t')
+	(user, date) = user.split('-')
+	date = directory + date + '.tsv'
+	if date not in fileList :
+		dateFile = open(date, 'w+')
+		fileList[date] = dateFile
+	fileList[date].write(user+'\t'+data)
