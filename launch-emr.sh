6606926408305d17d6eff1d3aa63f1f9a67efb83
Austin Gibbons <austin@ip-10-120-251-190.ec2.internal>
2012-05-15 18:39:06 +0000

    takes a mapper and a reducer

--- launch-emr.sh
+++ launch-emr.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
 
 if [ -z "$1" ]; then
-	echo "Usage: $0 [job name] [num instances total]"
+	echo "Usage: $0 [job name] [num instances total] [mapper] [reducer]"
 	exit
 fi
 
 if [ -z "$2" ]; then
-	echo "Usage: $0 [job name] [num instances total]"
+	echo "Usage: $0 [job name] [num instances total] [mapper] [reducer]"
+	exit
+fi
+
+if [ -z "$3" ]; then
+	echo "Usage: $0 [job name] [num instances total] [mapper] [reducer]"
+	exit
+fi
+
+if [ -z "$4" ]; then
+	echo "Usage: $0 [job name] [num instances total] [mapper] [reducer]"
 	exit
 fi
 
@@ -14,5 +24,4 @@ echo "push files to s3"
 s3cmd put *.py s3://mr-code-wiki/
 
 echo "launch emr job"
-/home/ubuntu/emr/elastic-mapreduce --create --name "user-$1" --stream --input s3://diffdb-wiki/ --output "s3://diffdb-output/output-$1/" --mapper s3://mr-code-wiki/map_user.py --reducer s3://mr-code-wiki/reducer_user.py --num-instances "$2" --master-instance-type "c1.medium"  --slave-instance-type "m1.xlarge" --enable-debugging
-
+/home/ubuntu/emr/elastic-mapreduce --create --name "user-$1" --stream --input s3://diffdb-wiki/ --output "s3://diffdb-output/output-$1/" --mapper s3://mr-code-wiki/$3 --reducer s3://mr-code-wiki/$4 --num-instances "$2" --master-instance-type "c1.medium"  --slave-instance-type "m1.xlarge" --enable-debugging
