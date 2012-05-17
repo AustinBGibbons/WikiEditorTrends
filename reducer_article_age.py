09b76d138842b4d440640721b5b2f4c7d5a97997
Susan Biancani <susan@ip-10-120-251-190.ec2.internal>
2012-05-17 17:27:32 +0000

    new version of reducer_article_age.py

--- reducer_article_age.py
+++ reducer_article_age.py
@@ -1,50 +1,66 @@
 #!/usr/bin/env python
 import sys
 
+SECONDS_PER_WEEK = 60*60*24*7
 
 '''
     @author : susan_biancani
-    outputs tab delimited text: page id, timestamp of first edit, time of each edit 
+    outputs tab delimited text: week, summary stats for age of edits in that week
+    summary stats = count, sum, sumsq
+    (Using count, sum, sumsq, you can calculate mean, st dev)
 '''
+
+def findFirstEdit(editTimes):
+    return min(editTimes)
+
+def collectWeekData(weeks, editTimes):
+    firstEdit = findFirstEdit(editTimes)
+    for edit in editTimes:
+        age = edit - firstEdit
+        if not weeks.has_key(edit):
+            weeks[edit] = [0.0, 0.0, 0.0]
+        weeks[edit][0]+=1
+        weeks[edit][1]+=age
+        agesq = weeks[edit][2]
+        weeks[edit][2]+=(age*age) 
+        if agesq > weeks[edit][2]:
+            sys.stderr.write("Underflow on " + str(firstEdit) + " " + str(edit))
 
 # Schema defined at : 
 #https://github.com/whym/RevDiffSearch/blob/master/README.rst
 get = {'rev_id':0, 'page_id':1, 'namespace':2, 'title':3, 'timestamp':4,
 'comment':5, 'minor':6, 'user_id':7, 'user_text':8}
 
-def processAndOutput(page, editTimes, timestamp):
-    if len(editTimes) == 0: 
-        print page, '\t', timestamp
-        return
-    else: 
-        first = min(editTimes)    
-        for time in editTimes:
-            output = [ page, str(first) ]
-            output.append(str(time))
-            print '\t'.join(output)
-
+"""Each week is a list: [count, sum(age of edits), sumsq(age of edits)]"""
+weeks = {}
 editTimes = []
 lastPage = -1
-#filename = "C:/Users/Susan/Documents/CS341/WikiEditor/subset_test.txt"
+#filename = "C:/Users/Susan/Documents/CS341/WikiEditor/subset.tsv"
 #data = open(filename)
-for line in sys.stdin :
 #for line in data.readlines():
+for line in sys.stdin :
     line = line.strip('\n').split('\t')
 
     # gather article statistics and output
     page = line[get['page_id']+1]
     timestamp = line[get['timestamp']+1]
+    weekTimestamp = int(timestamp)/SECONDS_PER_WEEK
     if lastPage != page:
         if lastPage < 0:
             lastPage = page
         else:
-            processAndOutput(lastPage, editTimes, timestamp)
+            #firstEdit = findFirstEdit(lastPage, editTimes)
+            collectWeekData(weeks, editTimes)
             lastPage = page
             editTimes = []
 
     # collect edit time for a revision
-    editTimes.append(int(line[get['timestamp']+1] ))
+    editTimes.append(weekTimestamp)
     
-processAndOutput(page, editTimes, timestamp)
+collectWeekData(weeks, editTimes)
+
+for week in weeks:
+    output = [str(w) for w in weeks[week]]
+    print '\t'.join(output)
 
 #data.close()
